82e621c6f10d1f1b825fec2d537d9c850fba2011
ellivainio <bgh301@myy.haaga-helia.fi>
2020-12-09 22:28:39 +0200

    new comment

--- src/main/java/model/dao/DBAccounts.java
+++ src/main/java/model/dao/DBAccounts.java
@@ -1,17 +1,7 @@
 package model.dao;
 
-public class DBAccounts {
-	
-    
-	
-	public static final String DBUSERNAME = "bgh301";
-	// dbkäyttäjätunnuksesi on tässä sama kuin bg/a-alkuinen HH-käyttäjätunnuksesi
-	
-	public static final String DBPASSWORD ="cuBISd87r";
-	// kts. mariadb.haaga-helia.fi-palvelimen juurihakemistosta
-	
-	public static final String DBURL = "jdbc:mysql://localhost:3306/bgh301";
-	// tietokantasi nimi on tässä sama bg/a-alkuinen HH-käyttäjätunnuksesi
-
-
+public class DBAccounts{
+public static final String DBUSERNAME = "vaihda_tähän_DB_käyttäjätunnuksesi";
+public static final String DBPASSWORD ="vaihda_tähän_DB_salasanasi";
+public static final String DBURL = "jdbc:mysql://localhost:3306/vaihda_tietokannan_nimi_tähän";
 }
